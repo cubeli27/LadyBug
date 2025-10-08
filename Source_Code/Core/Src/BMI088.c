a409d7b41e1d055598a726a00e12d5e38c42bc19
Jure Cubelic <134604815+cubeli27@users.noreply.github.com>
2025-10-08 15:01:03 +0200

    Update BMI088.c

--- Source_Code/Core/Src/BMI088.c
+++ Source_Code/Core/Src/BMI088.c
@@ -2,6 +2,13 @@
  * This file is based on work by Philip Salmony, 2019, under the BSD 3-Clause License.
  * Original source: https://github.com/pms67/HadesFCS
  *
+ * Modifications (c) 2024, Jure Cubelic 
+ * 
+ */
+/*
+ * This file is based on work by Philip Salmony, 2019, under the BSD 3-Clause License.
+ * Original source: https://github.com/pms67/HadesFCS
+ *
  * Modifications (c) 2025, Jure Cubelic 
  * 
  */
